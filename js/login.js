1abbc8a94e491d93b23aef39eae537122f05ccb3
v1nder <tsekhanovskiy@gmail.com>
2020-02-11 10:12:16 +0200

    Last changes: video content

--- js/login.js
+++ js/login.js
@@ -2,6 +2,18 @@ const linkLogin = document.querySelector(".user_nav-link");
 const popup = document.querySelector(".modal_login");
 const closeLogin = document.querySelector(".modal_login-close");
 const login = document.querySelector("[name=login]");
+const buttonTrailer = document.querySelector(".top_button-link");
+const trailerMain = document.querySelector(".video_trailer");
+
+buttonTrailer.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    trailerMain.classList.add("video_trailer-show");
+});
+
+closeLogin.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    trailerMain.classList.remove("video_trailer-show");
+});
 
 linkLogin.addEventListener("click", function (evt) {
     evt.preventDefault();
