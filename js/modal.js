d006f8d3f0aed761fa8de3d683b844cc71729752
v1nder <tsekhanovskiy@gmail.com>
2020-02-23 02:07:22 +0200

    Last changes: video content

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+const linkLogin = document.querySelector(".user_nav-link");
+const popup = document.querySelector(".modal_login");
+const closeLogin = document.querySelector(".modal_login-close");
+const login = document.querySelector("[name=login]");
+const buttonTrailer = document.querySelector(".top_button-link");
+const trailerMain = document.querySelector(".video_trailer");
+const overlay = document.querySelector(".modal_overlay");
+const closeVideo = document.querySelector(".modal_video-close");
+const videoButton = document.querySelector("[type=button]");
+
+
+buttonTrailer.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    trailerMain.classList.add("video_trailer-show");
+    overlay.classList.add("overlay_show");
+    videoButton.focus();
+});
+
+closeVideo.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    trailerMain.classList.remove("video_trailer-show");
+    overlay.classList.remove("overlay_show");
+});
+
+linkLogin.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal_login-show");
+    overlay.classList.add("overlay_show");
+    login.focus();
+});
+
+closeLogin.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    trailerMain.classList.remove("video_trailer-show");
+    popup.classList.remove("modal_login-show");
+    overlay.classList.remove("overlay_show");
+});
+
